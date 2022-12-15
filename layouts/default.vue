a03412f272d233d940a02618ff291ef7109bc2a6
Serhii Petrash <serhiipetrash@gmail.com>
2022-12-15 10:32:15 +0200

    + Navbar

---
+++ layouts/default.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <header>
+      <nav>
+        <NuxtLink to="/">Main</NuxtLink>
+        <NuxtLink to="/favorites">Favorites</NuxtLink>
+
+      </nav>
+    </header>
+
+    <slot />
+
+  </div>
+</template>
+
+<style>
+*,
+*::before,
+*::after{box-sizing:border-box;}
+a{text-decoration:none; color:inherit; cursor:pointer;}
+button{background-color:transparent; color:inherit; border-width:0; padding:0; cursor:pointer;}
+figure{margin:0;}
+input::-moz-focus-inner {border:0; padding:0; margin:0;}
+ul, ol, dd{margin:0; padding:0; list-style:none;}
+/* h1, h2, h3, h4, h5, h6{margin:0; font-size:inherit; font-weight:inherit;} */
+/* p{margin:0;} */
+cite {font-style:normal;}
+fieldset{border-width:0; padding:0; margin:0;}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  margin: 0;
+}
+header {
+  background-color: #6b5b95;
+}
+nav {
+  max-width: 1200px;
+  margin: 0 auto;
+  display: flex;
+  gap: 1rem;
+  padding: 1rem;
+  font-size: 1.5rem;
+  font-weight: bold;
+  color: white;
+}
+.router-link-active {
+  color:tomato
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  app: {
+    head: {
+      title: 'Open Weather app',
+      meta: [
+        {name: 'description', content: 'Open weather app'}
+      ],
+      link: [
+        {rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons'},
+        
+      ]
+    }
+  },
 })
